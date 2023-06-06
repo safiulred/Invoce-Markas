c6b8f6b0681012c41a2532d1dbd6af2c40731a72
Your Name <you@example.com>
2023-06-06 14:17:12 +0700

    .

--- config/db.js
+++ config/db.js
@@ -6,20 +6,11 @@ module.exports = {
         "username": "",
         "db": "invoice-markas"
     },
-<<<<<<< HEAD
-    production : {
-        "host" : "localhost",
-        "port" : 31015,
-        "password" : "",
-        "username" : "",
-        "db" : "invoice-markas"
-=======
     production: {
         "host": "localhost",
-        "port": 37027,
+        "port": 31015,
         "password": "",
         "username": "",
         "db": "invoice-markas"
->>>>>>> 9cd68184fe56f2046b1c4cbe7f87dc90fad8b2b9
     }
 }
--- config/redis.js
+++ config/redis.js
@@ -6,20 +6,11 @@ module.exports = {
         port: 12315,
         password: '{SHA512}d6+Wcw6WHqSzDR/Xj+kriXUHMorgfZGt7zuj3GusoOUuybLuIezOHwaNCRto7Bicdw8d9ft9HNldlThp0rV60w=='
     },
-<<<<<<< HEAD
-    production : {
-        secret:'markatnett',
-        sessionName : 'invoicenet',
-        host : 'localhost',
-        port : 5470,
-        password : ''
-=======
     production: {
         secret: 'markatnett',
         sessionName: 'invoicenet',
         host: 'localhost',
         port: 6379,
         password: ''
->>>>>>> 9cd68184fe56f2046b1c4cbe7f87dc90fad8b2b9
     }
 }
